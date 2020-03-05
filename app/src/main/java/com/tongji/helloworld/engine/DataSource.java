7c087326ee04ffea46bb2996073dadcc91303f01
saturnluohao <3099916791@qq.com>
2020-03-05 14:12:30 +0800

    data from flightradar(without heapmap)

--- app/src/main/java/com/tongji/helloworld/engine/DataSource.java
+++ app/src/main/java/com/tongji/helloworld/engine/DataSource.java
@@ -27,6 +27,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -133,8 +134,8 @@ public class DataSource {
         BitmapDescriptor bitmap = new BitmapDescriptorFactory().fromResource(R.drawable.airplane);
         LatLng neBound = mBaiduMap.getMapStatus().bound.northeast;
         LatLng swBound = mBaiduMap.getMapStatus().bound.southwest;
-        String flightInfoData = getFlightInfoData(swBound.longitude, neBound.longitude, swBound.latitude, neBound.latitude);
-        List<FlightInfo> flightInfoList = parseData(flightInfoData);
+        String flightInfoData = getFlightInfoDataFromRadar(swBound.longitude, neBound.longitude, swBound.latitude, neBound.latitude);
+        List<FlightInfo> flightInfoList = parseDataFramRadar(flightInfoData);
         markerUpdater.updateOverlayMap(flightInfoList, mBaiduMap);
     }
 
@@ -145,6 +146,34 @@ public class DataSource {
 //            URL url = new URL(host + "/flightInfo" + queryString);
             String queryString = "minLon=" + minLon + "&maxLon=" + maxLon + "&minLat=" + minLat + "&maxLat=" + maxLat + "&token=" + token;
             URL url = new URL(queryurl + queryString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            if (urlConnection.getResponseCode() != 200) {
+                return "";
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection
+                    .getInputStream(), "utf-8"));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+            urlConnection.disconnect();
+        }catch (java.io.IOException e) {
+            e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json.toString();
+    }
+
+    private String getFlightInfoDataFromRadar(double minLon, double maxLon, double minLat, double maxLat){
+        StringBuilder json = new StringBuilder();
+        try {
+            String queryString = String.format("https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=%f,%f,%f,%f&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1",
+                    maxLat, minLat, minLon, maxLon);
+            URL url = new URL(queryString);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
             if (urlConnection.getResponseCode() != 200) {
@@ -180,6 +209,31 @@ public class DataSource {
                 int altitude = prop.optInt("altitude", 0);
                 int direction = prop.getInt("direction");
                 String icao = prop.getString("flight_id");
+                FlightInfoList.add(new FlightInfo(icao, lat, lng, altitude, direction));
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return FlightInfoList;
+    }
+
+    private ArrayList<FlightInfo> parseDataFramRadar(String data){
+        ArrayList<FlightInfo> FlightInfoList = new ArrayList<FlightInfo>();
+        try {
+            JSONObject flightInfo = new JSONObject(data);
+            flightInfo.remove("full_count");
+            flightInfo.remove("version");
+            flightInfo.remove("stats");
+            Iterator<String> keys = flightInfo.keys();
+
+            while(keys.hasNext()){
+                String key = keys.next();
+                JSONArray item = flightInfo.getJSONArray(key);
+                String icao = item.getString(0);
+                Double lat = item.getDouble(1);
+                Double lng = item.getDouble(2);
+                int direction = item.getInt(3);
+                int altitude = item.getInt(4);
                 FlightInfoList.add(new FlightInfo(icao, lat, lng, altitude, direction));
             }
         }catch (JSONException e){
